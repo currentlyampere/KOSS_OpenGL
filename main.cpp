55b3a8ca75e7a086fc10bec86dc67c66950f6dda
Sudhanshu Tiwari <tiwarisudhanshu2006@gmail.com>
2026-04-04 21:49:30 +0530

    Defining VAO

--- main.cpp
+++ main.cpp
@@ -30,6 +30,8 @@ int main() {
     -0.5f, -0.5f, 0.0f,   // left
      0.5f, -0.5f, 0.0f    // right
     };
+/*
+*/
 //(VBO)Sending this data to a memory block in GPU(buffer)
     unsigned int VBO;                       //creating ID for Buffer
     glGenBuffers(1, &VBO);                  // creating buffer and storing its ID
@@ -37,8 +39,16 @@ int main() {
     //Filling the buffer with data
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     //                                                          its gonna be const. data
+//(VAO) Telling GPU how to read that data via memory block containing instructions
+    unsigned int VAO;                       //creating ID containing that memory block
+    glGenVertexArrays(1, &VAO);             // creating memory block and storing its ID
+    glBindVertexArray(VAO);                 //selecting this vertex array
 
-    //RENDER LOOP
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);      //telling vertex array to use this vertex buffer
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // real instructions to how to read attributes
+    glEnableVertexAttribArray(0);               //turning on attribute on location 0
+    //RENDER LOOP    
     while (!glfwWindowShouldClose(window)) 
     {      
         glClearColor(0.1f, 0.1f, 0.15f, 1.0f); 
